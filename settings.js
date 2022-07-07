57eecc225965f8c7b2afbef1c0a1f837fe4a72e5
RuminasVT2 <a85198201@gmail.com>
2022-07-07 17:30:02 +0800

    Felicia New Era

---
+++ settings.js
@@ -0,0 +1,147 @@
+//════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Xeon Bot Inc. Cheems Bot MD
+//Thank you to Lord Buddha, Family and Myself
+//════════════════════════════//
+//recode kar ke youtube pe upload kar rhe hai ya
+//codes copy kar ke apne script me dal rhe
+//hai to, description me xeon ka yt channel
+// ka link paste kr dena as a cradit or github 
+//repo me bhi tag kardena baki jo
+//bhi karna hai apki marzi, thank you!🦄
+//════════════════════════════//
+//If you recode and uploading on your channel
+//or copy pasting the codes in ur script, 
+//i give permission to do as long as you
+//put Xeons youtube channel link in the video
+//description and tag me on githuh repo, 
+//thank you🦄
+//════════════════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'hdiiofficial',
+}
+
+
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//semt menu variants
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.vcardowner = ['6281347927862'] //ur owner number
+global.ownername = "⌬ Clara-DV" //ur owner name
+global.ytname = "YT: ⌬ Clara-DV" //ur yt chanel name
+global.socialm = "Instagram: katyushaclara" //ur github or insta name
+global.location = "Indonesia Bagian Tengah" //ur location
+
+//bot bomdy 
+global.owner = ['6281347927862']
+global.ownertag = '6281347927862' //ur tag number
+global.botname = '⌬ Felicia-MD' //ur bot name
+global.linkz = "https://www.instagram.com/katyushaclara/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.instagram.com/katyushaclara/" //ur website to be displayed
+global.botscript = 'https://www.instagram.com/katyushaclara/' //script link
+global.reactmoji = "🛐" //ur menu react emoji
+global.themeemoji = " • " //ur theme emoji
+global.packname = "Created By Bot Felicia\n\n\n\n\n\n\n\n\n\n\n. " //ur sticker watermark packname
+global.author = "6282253479547" //ur sticker watermark author
+
+//Bot theme media
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//damtabase
+global.premium = ['6281347927862'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '*〔✅〕Selesai*',
+    admin: '*〔❗〕Fitur Khusus Untuk Admin Grup*',
+    botAdmin: '*〔❗〕Jadikan Bot Sebagai Admin Dulu*',
+    owner: '*〔❗〕Fitur Khusus Owner Bot Ini*',
+    group: '*〔❗〕Fitur Khusus Di Grup*',
+    private: '*〔❗〕Fitur Khusus Di Chat Pribad*',
+    bot: '*〔❗〕Fitur Ini Khusus Untuk Bot Felicia*',
+    wait: '*〔❗〕Proses*',
+    linkm: '*〔❗〕Sertakan Link Setelah Command*',
+    error: '*〔❗〕Error*',
+    endLimit: '*Batas Harian Anda Telah Habis, Batas Akan Direset Setiap 12 Jam*',
+    ban: '*Anda Telah Diblokir Oleh Owner, Jika Anda Ingin Membatalkan Blokir Silahkan Chat Owner*',
+    nsfw: '*Fitur Nsfw Belum Di Aktifkan, Silahkan Hubungi Admin Untuk Di-Aktifkan*',
+    banChat: '*Bot Diblokir Di Grup Ini, Harap Hubungi Pemiliknya Untuk Membatalkan Pemblokiran*'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 25,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=discovery-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=uprise-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=star-wars-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=trance-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
