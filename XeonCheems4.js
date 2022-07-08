d48daae9b625a0acfdfda3dd90d644d8e3552927
RuminasVT2 <107530906+RuminasVT2@users.noreply.github.com>
2022-07-08 16:20:03 +0800

    Add files via upload

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -8061,7 +8061,7 @@ sourceUrl: anu.url
 XeonBotInc.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
 break
-case 'getmusic': case 'getvideo': case 'yt': case 'youtube': case ': {
+case 'getmusic': case 'getvideo': case 'yt': case 'youtube': {
    if (isBan) return reply(mess.ban)	 			
 if (isBanChat) return reply(mess.banChat)
 if (!args[0]) return reply(mess.linkm)
